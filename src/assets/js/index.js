23a6287fdacf16d4001e46146f0c82181e782026
andre <61792917+luuxis@users.noreply.github.com>
2024-03-29 12:23:56 +0100

    Initial commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,144 @@
+/**
+ * @author Luuxis
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
+ */
+
+const { ipcRenderer } = require('electron');
+const pkg = require('../package.json');
+const os = require('os');
+import { config, database } from './utils.js';
+const nodeFetch = require("node-fetch");
+
+
+class Splash {
+    constructor() {
+        this.splash = document.querySelector(".splash");
+        this.splashMessage = document.querySelector(".splash-message");
+        this.splashAuthor = document.querySelector(".splash-author");
+        this.message = document.querySelector(".message");
+        this.progress = document.querySelector(".progress");
+        document.addEventListener('DOMContentLoaded', async () => {
+            let databaseLauncher = new database();
+            let configClient = await databaseLauncher.readData('configClient');
+            let theme = configClient?.launcher_config?.theme || "auto"
+            let isDarkTheme = await ipcRenderer.invoke('is-dark-theme', theme).then(res => res)
+            document.body.className = isDarkTheme ? 'dark global' : 'light global';
+            if (process.platform == 'win32') ipcRenderer.send('update-window-progress-load')
+            this.startAnimation()
+        });
+    }
+
+    async startAnimation() {
+        let splashes = [
+            { "message": "Je... vie...", "author": "Luuxis" },
+            { "message": "Salut je suis du code.", "author": "Luuxis" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+        ];
+        let splash = splashes[Math.floor(Math.random() * splashes.length)];
+        this.splashMessage.textContent = splash.message;
+        this.splashAuthor.children[0].textContent = "@" + splash.author;
+        await sleep(100);
+        document.querySelector("#splash").style.display = "block";
+        await sleep(500);
+        this.splash.classList.add("opacity");
+        await sleep(500);
+        this.splash.classList.add("translate");
+        this.splashMessage.classList.add("opacity");
+        this.splashAuthor.classList.add("opacity");
+        this.message.classList.add("opacity");
+        await sleep(1000);
+        this.checkUpdate();
+    }
+
+    async checkUpdate() {
+        return this.dowloadUpdate();
+        this.setStatus(`Recherche de mise à jour...`);
+
+        ipcRenderer.invoke('update-app').then().catch(err => {
+            return this.shutdown(`erreur lors de la recherche de mise à jour :<br>${err.message}`);
+        });
+
+        ipcRenderer.on('updateAvailable', () => {
+            this.setStatus(`Mise à jour disponible !`);
+            if (os.platform() == 'win32') ipcRenderer.send('start-update');
+            else return this.dowloadUpdate();
+        })
+
+        ipcRenderer.on('error', (event, err) => {
+            if (err) return this.shutdown(`${err.message}`);
+        })
+
+        ipcRenderer.on('download-progress', (event, progress) => {
+            this.toggleProgress();
+            ipcRenderer.send('update-window-progress', { progress: progress.transferred, size: progress.total })
+            this.setProgress(progress.transferred, progress.total);
+        })
+
+        ipcRenderer.on('update-not-available', () => {
+            console.error("Mise à jour non disponible");
+            this.maintenanceCheck();
+        })
+    }
+
+    async dowloadUpdate() {
+        const repoURL = pkg.repository.url.replace("git+", "").replace(".git", "").replace("https://github.com/", "").split("/");
+        const githubAPI = await nodeFetch('https://api.github.com').then(res => res.json()).catch(err => err);
+        
+        const githubAPIRepoURL = githubAPI.repository_url.replace("{owner}", repoURL[0]).replace("{repo}", repoURL[1]);
+        const githubAPIRepo = await nodeFetch(githubAPIRepoURL).then(res => res.json()).catch(err => err);
+
+        const releases_url = githubAPIRepo.releases_url.replace("{/id}", '');
+
+
+    }
+        
+
+    async maintenanceCheck() {
+        config.GetConfig().then(res => {
+            if (res.maintenance) return this.shutdown(res.maintenance_message);
+            this.startLauncher();
+        }).catch(e => {
+            console.error(e);
+            return this.shutdown("Aucune connexion internet détectée,<br>veuillez réessayer ultérieurement.");
+        })
+    }
+
+    startLauncher() {
+        this.setStatus(`Démarrage du launcher`);
+        ipcRenderer.send('main-window-open');
+        ipcRenderer.send('update-window-close');
+    }
+
+    shutdown(text) {
+        this.setStatus(`${text}<br>Arrêt dans 5s`);
+        let i = 4;
+        setInterval(() => {
+            this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
+            if (i < 0) ipcRenderer.send('update-window-close');
+        }, 1000);
+    }
+
+    setStatus(text) {
+        this.message.innerHTML = text;
+    }
+
+    toggleProgress() {
+        if (this.progress.classList.toggle("show")) this.setProgress(0, 1);
+    }
+
+    setProgress(value, max) {
+        this.progress.value = value;
+        this.progress.max = max;
+    }
+}
+
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
+        ipcRenderer.send("update-window-dev-tools");
+    }
+})
+new Splash();
